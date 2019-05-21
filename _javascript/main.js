ddd5132146e10e60bbb8387e08b9d5ae8cfb08f1
Ville Vuorenmaa <villevuor@gmail.com>
2019-05-21 10:06:51 +0300

    ajax

--- _javascript/main.js
+++ _javascript/main.js
@@ -22,26 +22,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // console.log(values);
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", 'https://muflon.fi/leirinjohtaja.php', true);
+    axios.post('https://muflon.fi/leirinjohtaja.php', {data: values})
+    .then(function (response) {
+      console.log(response);
 
-    xhr.setRequestHeader("Content-Type", "application/json");
+      var results = [
+        'Onneksi olkoon, olet selvästi leirinjohtajaehdokasmateriaalia! Vastaamasi kysymykset ovat juuri sellaisia, joita Finnjamboreen johdossa joutuu miettimään. Tuleva suurleiri tulee väistämättä vaikuttamaan partioon Suomessa ja sinulla olisi nyt ainutkertainen mahdollisuus jättää kädenjälkesi sen historiaan.',
+        'Olet selkeästi pohtinut näitä juttuja! Vaikuttaisi siltä, että sinun kannattaisi hakea finnjamboreen johtajaksi. Suurleiri on suomalaisen partion suurin ja monipuolisin johtamistehtävä, jossa pääset varmasti haastamaan itseäsi tällaisten arvokysymystenkin parissa.',
+        'Tästä se lähtee: perusperiaatteet kuntoon ja tavoitteita luomaan! Suurleirin reunaehtoja määritellään tulevien leirinjohtajien kanssa. Jos haluat saada siis äänesi vielä paremmin kuuluviin, niin hae leirinjohtajaksi!'
+      ]
 
-    xhr.onreadystatechange = function() {
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
-        var results = [
-          'Onneksi olkoon, olet selvästi leirinjohtajaehdokasmateriaalia! Vastaamasi kysymykset ovat juuri sellaisia, joita Finnjamboreen johdossa joutuu miettimään. Tuleva suurleiri tulee väistämättä vaikuttamaan partioon Suomessa ja sinulla olisi nyt ainutkertainen mahdollisuus jättää kädenjälkesi sen historiaan.',
-          'Olet selkeästi pohtinut näitä juttuja! Vaikuttaisi siltä, että sinun kannattaisi hakea finnjamboreen johtajaksi. Suurleiri on suomalaisen partion suurin ja monipuolisin johtamistehtävä, jossa pääset varmasti haastamaan itseäsi tällaisten arvokysymystenkin parissa.',
-          'Tästä se lähtee: perusperiaatteet kuntoon ja tavoitteita luomaan! Suurleirin reunaehtoja määritellään tulevien leirinjohtajien kanssa. Jos haluat saada siis äänesi vielä paremmin kuuluviin, niin hae leirinjohtajaksi!'
-        ]
+      document.getElementById('result').innerText = results[Math.floor(Math.random()*(3))];
 
-        document.getElementById('result').innerText = results[Math.floor(Math.random()*(3))];
-
-        document.getElementById('part1').className = 'content hidden';
-        document.getElementById('part2').className = 'content';
-      }
-    }
-
-    xhr.send(JSON.stringify({data: values}));
+      document.getElementById('part1').className = 'content hidden';
+      document.getElementById('part2').className = 'content';
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 });
